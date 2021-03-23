4dccdcd35314baebe2e4c3c98d22ff216dbeb70a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-03-23 22:06:31 +0100

    dod rozw zad 5

---
+++ part1_13_03_2021/z5/ChooseLongestString.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ChooseLongestString {
+
+    private static String[] words1 = { "Politechnika1", "Karol",
+				       "Adam", "Rogowski",
+				       "Politechnika2", "Super", "Weekend"};
+
+    private static String getLongestStrings(String[] words) {
+	String result = "";
+	for (int i = 0; i < words.length; i++) {
+	    if(result.length() == words[i].length()) {
+		result += ", " + words[i];
+	    } else if(result.length() < words[i].length()) {
+		result = words[i];
+	    }
+	}
+	return result;
+    }
+
+    public static void main(String[] args) {
+
+	System.out.println("Searching for the longest string in:");
+	System.out.println(Arrays.toString(words1));
+
+	System.out.println("The following word(s) is/are found:");
+	System.out.println(getLongestStrings(words1));
+    }
+}
+
+
---
+++ part1_13_03_2021/z5/README.md
@@ -0,0 +1,21 @@
+# Info
+
+Tresci zadan skopiowane z tych ktore dostalismy od Karola na podyplomowki JS Developer 2020/2021.
+
+Zadania z dnia 17-10-2020
+
+W ramach treningu rozwiaze je w Haskellu.
+
+# Task 5
+
+Choose longest string from the array. [‘Karol’, ‘Adam’,’Rogowski’,’Politechnika’,’Super’,’Weekend’].
+
+tylko 1, czy kilka w przypadku remisow?! (wezme kilka)
+
+# Uruchomienie
+
+```bash
+java chooseLongestString.java
+```
+
+i voila.
