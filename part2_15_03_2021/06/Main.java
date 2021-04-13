a5dd456d9f079f2291ac8c3f69aece99659dae29
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-13 10:37:38 +0200

    dodano rozw zad 6

---
+++ part2_15_03_2021/06/Main.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Main {
+
+    private static int[] sumOfPairs(int[] nums) {
+
+	boolean isLenOdd = (nums.length % 2 == 1);
+	int resultLen = isLenOdd ? (int) (nums.length/2+1) : nums.length/2;
+	int[] result = new int[resultLen];
+
+	for (int i = 0; i < result.length; i++) {
+	    if(isLenOdd && (i == (resultLen - 1))) {
+		result[i] = nums[i*2] * 2;
+	    } else {
+		result[i] = nums[i*2] + nums[i*2+1];
+	    }
+	}
+
+	return result;
+     }
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 1, 6, 23, 8, 4, 8, 3, 7 };
+		int[] numsAddedByPairs = sumOfPairs(numbers);
+
+		System.out.println("Original array: " +
+				   Arrays.toString(numbers));
+		System.out.println("After adding numbers in pairs: " +
+				   Arrays.toString(numsAddedByPairs));
+
+		System.out.printf("===%n");
+
+		int[] numbers2 = { 1, 6, 23, 8, 4, 8, 3 };
+		int[] numsAddedByPairs2 = sumOfPairs(numbers2);
+
+		System.out.println("Original array: " +
+				   Arrays.toString(numbers2));
+		System.out.println("After adding numbers in pairs: " +
+				   Arrays.toString(numsAddedByPairs2));
+	}
+}
---
+++ part2_15_03_2021/06/README.md
@@ -0,0 +1,38 @@
+# Info
+
+Tresci zadan skopiowane z tych ktore dostalismy od Karola na podyplomowki JS Developer 2020/2021.
+
+Zadania z dnia 24-10-2020
+
+W ramach treningu rozwiaze je w Java.
+
+# Moj obecny background
+
+Tresci zadan skopiowane z tych ktore dostalismy od Karola na podyplomowki JS Developer 2020/2021.
+
+Zadania z dnia 24-10-2020
+
+W ramach treningu rozwiaze je w Java.
+
+
+# Tasks
+
+ARRAY: {1, 6, 23, 8, 4, 8, 3, 7}
+
+## Task 6
+
+For time of this example remove last element from the given array. Create a function that based on given array return new array in pattern [a,b,c,d,e] -> [a+b, c+d, e+e]
+
+
+# Uruchomienie
+
+```bash
+java Main.java
+```
+
+# Info dodatkowe
+
+## Do użytku własnego, nie zalecane do użytku przez nikogo innego.
+
+## For personal use only, not recommended to be used by anyone else.
+
